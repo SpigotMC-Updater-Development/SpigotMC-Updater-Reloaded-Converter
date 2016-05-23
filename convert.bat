fe36f43f549a222859b4b1d8773c492088711a10
ShadowCable <ShadowCable@thegearmc.com>
2016-05-23 14:54:40 -0600

    corrected for running in tasks folder

--- convert.bat
+++ convert.bat
@@ -2,35 +2,32 @@
 
 set startdir=%~dp0
 
-@echo Updating %startdir%SpigotMC_Updater.bat
-del /f SpigotMC_Updater.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/SpigotMC_Updater.bat -OutFile SpigotMC_Updater.bat
-@echo Updated %startdir%SpigotMC_Updater.bat
+cd %startdir%
+
+@echo Updating SpigotMC_Updater.bat
+del /f ..\SpigotMC_Updater.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/SpigotMC_Updater.bat -OutFile ..\SpigotMC_Updater.bat
+@echo Updated SpigotMC_Updater.bat
 echo.
-@echo Updating %startdir%menu.bat
-del /f menu.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/menu.bat -OutFile menu.bat
-@echo Updated %startdir%menu.bat
+@echo Updating menu.bat
+del /f ..\menu.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/menu.bat -OutFile ..\menu.bat
+@echo Updated menu.bat
 echo.
-@echo Updating %startdir%runbuildtools.bat
-del /f runbuildtools.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/runbuildtools.bat -OutFile runbuildtools.bat
+@echo Updating tasks\Buildtools_Files\run.bat
+del /f tasks\Buildtools_Files\run.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/tasks/Buildtools_Files/run.bat -OutFile Buildtools_Files\run.bat
 @echo Updated %startdir%runbuildtools.bat
 echo.
-@echo Updating %startdir%plugin_repair_tool.bat
+@echo Updating tasks\plugin_repair_tool.bat
 del /f plugin_repair_tool.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/runbuildtools.bat -OutFile runbuildtools.bat
-@echo Updated %startdir%plugin_repair_tool.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/tasks/plugin_repair_tool.bat -OutFile plugin_repair_tool.bat
+@echo Updated task\plugin_repair_tool.bat
 echo.
-@echo Updating %startdir%tasks\delbt.bat
-del /f tasks\delbt.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/tasks/delbt.bat -OutFile tasks/delbt.bat
-@echo Updated %startdir%tasks\delbt.bat
-echo.
-@echo Updating %startdir%tasks\help.bat
-del /f tasks\help.bat
-powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/tasks/help.bat -OutFile tasks/help.bat
-@echo Updated %startdir%tasks\help.bat
+@echo Updating tasks\delbt.bat
+del /f delbt.bat
+powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded/master/tasks/delbt.bat -OutFile delbt.bat
+@echo Updated tasks\delbt.bat
 echo.
 
 @echo Checking for any new files to add/remove...
@@ -39,9 +36,9 @@ echo.
 echo.
 powershell -command Invoke-WebRequest -Uri https://raw.githubusercontent.com/SpigotMC-Updater-Development/SpigotMC-Updater-Reloaded-Converter/master/tasks/changelog.bat -OutFile tasks\changelog.bat
 @echo Changes in this version:
-start /b /wait tasks\changelog.bat
+start /b /wait changelog.bat
 echo.
-@echo You can read the changelogs by issuing command "program changes" in the program ;)
+@echo You can read the changelogs at anytime in the program ;)
 pause
 
 exit
